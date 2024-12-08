3e78578150305fcabaf6af72175a139c1484321f
Shockwave_ZA <mpramlall@gmail.com>
2024-12-08 12:48:22 +0200

    Extended layout with dropdown for other days

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,11 +18,6 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
     public IActionResult Day(int day)
     {
         ISolution solution = new Day1();
